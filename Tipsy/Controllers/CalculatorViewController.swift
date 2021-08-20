282bd180f9bf57eaedd3753f76276e0233abcdb3
kantzz <codingblyatt@gmail.com>
2021-08-20 19:01:18 +0500

    Tweaking variables in CalculatorViewController and retrieving data

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,39 +15,59 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    var tipAmount: String!
+    var tipString: String!
+    var tipAmount: Float = 0.0
+    var stepperValue: Float = 0.0
+    var bill: Float = 0.0
     
     
     
     @IBAction func tipChanged(_ sender: UIButton) {
         updateUI()
         sender.isSelected = true
-        print(sender.currentTitle!)
-        tipAmount = sender.currentTitle!
+//        print(sender.currentTitle!)
+        tipString = sender.currentTitle!
+        
+        let billText = billTextField.text ?? "0.0"
+        
+        bill = (billText as NSString).floatValue
+        billTextField.endEditing(true)
         
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        sender.minimumValue = 0
+        sender.stepValue = 1
+//        print(sender.value)
+        stepperValue = Float(sender.value)
+        updateUI()
+        
+        
     
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        if tipAmount == "10%"{
-            let tipTest = "0.1"
-            print(tipTest)
-        } else if tipAmount == "20%"{
-            let tipTest = "0.2"
-            print(tipTest)
+        if tipString == "10%"{
+            tipAmount = 0.1
+        } else if tipString == "20%"{
+            tipAmount = 0.2
         } else {
-            let tipTest = "0.0"
-            print(tipTest)
+            tipAmount = 0.0
         }
+        
+        print(tipAmount)
+        
+        print(stepperValue)
+        
+        print(bill)
+        
     }
     
     func updateUI(){
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
+        splitNumberLabel.text = String(format: "%.0f", stepperValue)
     }
     
     
